f5117c1e88d02c5ad6fc14293f20feffd25cd52c
Lars <itsmelmw@gmail.com>
2022-05-11 12:46:12 +0200

    added check for same name and error messagers

--- connect4/StartScreen.cs
+++ connect4/StartScreen.cs
@@ -13,13 +13,14 @@ namespace Connect4
         }
         public (Player, Player) Play()
         {
-            string p1Name = p1NameSelection.Play(1);
+            string p1Name = p1NameSelection.Play(1, null);
             ConsoleColor p1Color = p1ColorSelection.Play(p1Name, null);
 
-            string p2Name = p2NameSelection.Play(2);
-            ConsoleColor p2Color = p2ColorSelection.Play(p2Name, p1Color);
-
             Player p1 = new Player(p1Name, p1Color);
+
+            string p2Name = p2NameSelection.Play(2, p1);
+            ConsoleColor p2Color = p2ColorSelection.Play(p2Name, p1);
+
             Player p2 = new Player(p2Name, p2Color);
             return (p1, p2);
         }
@@ -27,29 +28,38 @@ namespace Connect4
 
     class NameSelection
     {
-        public string Play(int number)
+        string error;
+        public NameSelection()
+        {
+            error = "";
+        }
+        public string Play(int number, Player? other)
         {
             string name = "";
-            Console.Clear();
+
             while (true)
             {
-                Console.SetCursorPosition(Console.WindowWidth / 2 - 8, Console.WindowHeight / 2 - 1);
-                Console.Write("Player {0}'s name:", number);
+                Console.Clear();
+                Draw(number);
                 Console.SetCursorPosition(Console.WindowWidth / 2 - 8, Console.WindowHeight / 2 + 1);
                 name = Console.ReadLine() ?? "";
-                if (isValid(name))
+                if (isValid(name, other))
                     return name;
                 else
-                {
-                    Console.Clear();
-                    Console.SetCursorPosition(Console.WindowWidth / 2 - 7, Console.WindowHeight / 2 - 3);
-                    Console.Write("Invalid name!");
-                }
+                    error = "Invalid name!";
             }
         }
-        private bool isValid(string name)
+        public void Draw(int number)
         {
-            if (name.Length == 0 || name.Length > 16)
+            string text = "Player " + number + "'s name:";
+            Console.SetCursorPosition(Console.WindowWidth / 2 - error.Length / 2, Console.WindowHeight / 2 - 3);
+            Console.Write(error);
+            Console.SetCursorPosition(Console.WindowWidth / 2 - text.Length / 2, Console.WindowHeight / 2 - 1);
+            Console.Write(text);
+        }
+        private bool isValid(string name, Player? other)
+        {
+            if (name.Length == 0 || name.Length > 16 || (other != null && name == other.name))
                 return false;
             foreach (char c in name)
                 if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
@@ -60,10 +70,12 @@ namespace Connect4
 
     class ColorSelection
     {
+        string error;
         int selection;
         ScreenOption[] options;
         public ColorSelection()
         {
+            error = "";
             selection = 0;
             options = new ScreenOption[4];
             options[0] = new ScreenOption("Red", ConsoleColor.Red);
@@ -71,7 +83,7 @@ namespace Connect4
             options[2] = new ScreenOption("Green", ConsoleColor.Green);
             options[3] = new ScreenOption("Blue", ConsoleColor.Blue);
         }
-        public ConsoleColor Play(string name, ConsoleColor? forbidden)
+        public ConsoleColor Play(string name, Player? other)
         {
             int currentWidth = 0, currentHeight = 0;
 
@@ -103,12 +115,13 @@ namespace Connect4
                                 selection = 0;
                             break;
                         case ConsoleKey.Enter:
-                            if (options[selection].color != forbidden)
+                            if (other == null || options[selection].color != other.color)
                                 return options[selection].color;
+                            else
+                                error = "Color cannot be the same";
                             break;
                         case ConsoleKey.Escape:
-                            if (options[selection].color != forbidden)
-                                return options[selection].color;
+                            Program.CloseProgram();
                             break;
                         default:
                             break;
@@ -129,6 +142,9 @@ namespace Connect4
             else
             {
                 string text = name + "'s color:";
+                Console.SetCursorPosition(Console.WindowWidth / 2 - error.Length / 2, Console.WindowHeight / 2 - 6);
+                Console.Write(error);
+                
                 Console.SetCursorPosition(Console.WindowWidth / 2 - text.Length / 2, Console.WindowHeight / 2 - 4);
                 Console.Write(text);
 
